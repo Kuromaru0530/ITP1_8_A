858fd7d6878b364800de8e7e88afa49a00a886a2
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:27:45 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP8_1_A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ITP1_8_A
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Line = Console.ReadLine();
+            List<string> lines = new List<string>();
+
+            foreach(char c in Line)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (char.IsUpper(c))
+                    {
+                        lines.Add(c.ToString().ToLower());
+                    }
+                    else if (char.IsLower(c))
+                    {
+                        lines.Add(c.ToString().ToUpper());
+                    }
+                }
+                else lines.Add(c.ToString());
+            }
+            string Ans = string.Join("", lines.ToArray());
+            Console.WriteLine(Ans);
+        }
+    }
+}
